aca29f84ca65a6d8a3f61188a7b9031ec38b4a53
yong <1198749126@qq.com>
2019-04-08 02:26:50 -0700

    first commit

---
+++ hello-word/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user (id,username,name,age,balance) values (1,'account1','aaa',20,100.00);
+insert into user (id,username,name,age,balance) values (2,'account2','bbb',30,200.00);
+insert into user (id,username,name,age,balance) values (3,'account3','ccc',30,300.00);
