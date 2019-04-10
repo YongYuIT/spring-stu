4c3ee958fb3c955ab28d5ed3e63430690a2f9042
yong <1198749126@qq.com>
2019-04-09 20:08:05 -0700

    mybatis-gen

---
+++ hello-mybatis/src/test/java/com/thinking/hellomybatis/HelloMybatisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thinking.hellomybatis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloMybatisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
