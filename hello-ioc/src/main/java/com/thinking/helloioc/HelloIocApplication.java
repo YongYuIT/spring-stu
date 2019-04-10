c242f087d5cde22bec099386e45fd09cbe2444e0
yong <1198749126@qq.com>
2019-04-10 02:33:58 -0700

    record

---
+++ hello-ioc/src/main/java/com/thinking/helloioc/HelloIocApplication.java
@@ -0,0 +1,13 @@
+package com.thinking.helloioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloIocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloIocApplication.class, args);
+    }
+
+}
