ff8fa0ea241f5379138bfefb08015e8539285569
yong <1198749126@qq.com>
2019-04-10 02:32:34 -0700

    record

---
+++ hello-mvc/src/main/java/com/thinking/hellomvc/RequestParamController.java
@@ -0,0 +1,31 @@
+package com.thinking.hellomvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/requestparam")
+public class RequestParamController {
+
+    @PostMapping("/test")
+    @ResponseBody
+    public Map getPatams(@RequestParam String name,
+                         @RequestParam int id,
+                         @RequestParam(defaultValue = "100") int age,
+                         @RequestParam(required = false) Character gender) {
+        Map params = new HashMap();
+        params.put("des", "hello");
+        params.put("name", name);
+        params.put("id", id);
+        params.put("age", age);
+        params.put("gender", gender);
+        return params;
+    }
+
+}
